745861a9e617d316a38bd87c4ac9ae213eb9480c
chanhpark <chanhyun.park1211@gmail.com>
2014-11-01 21:49:05 -0400

    used the wrong back slack. will take out the “new line”

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -4,8 +4,8 @@ puts 'pick a number between 1 and 1000'
 response = gets.chomp.to_i
 
 until response == number do #keep running program until the response matches the number (which is random)
-	if response < number then puts 'Too Low!/nTry again!' #if the response is lower than the number. Output is too low.
-		elsif response > number then puts 'Too High!/nTry again!'#if the response is higher than the number. Ouput is too high.
+	if response < number then puts 'Too Low! Try again!' #if the response is lower than the number. Output is too low.
+		elsif response > number then puts 'Too High Try again!'#if the response is higher than the number. Ouput is too high.
 	end
 		response = gets.chomp.to_i #will ask for a response after each if/elsif statement.
 end
